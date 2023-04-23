1ab06d1043b4bd12b1dd0d28465a1b6fc79ad330
mz0ng <110255630+mz0ng@users.noreply.github.com>
2023-04-22 19:47:12 -0700

    Update edfs.py

--- edfs.py
+++ edfs.py
@@ -3,6 +3,8 @@ import socket
 import sys
 import os
 import pathlib
+import subprocess
+import time
 
 
 def main(argvs):
@@ -10,6 +12,7 @@ def main(argvs):
     port = 5050
     ip_addr = socket.gethostbyname(socket.gethostname())
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     client.connect((ip_addr, port))
     # process command case by case
     if argvs[0] == '-ls':
@@ -99,9 +102,19 @@ def main(argvs):
             if ind == -1:
                 raise ValueError(
                     'Please specify path to a file instead of directory.')
+    elif argvs[0] == '-fs':
+        if len(argvs) != 2:
+            raise ValueError(
+                'Please follow the format: -fs [path to file on server]')
+        else:
+            f = argvs[1]
+            ind = f.rfind('.')
+            if ind == -1:
+                raise ValueError(
+                    'Please specify path to a file instead of directory.')
     else:
         raise ValueError(
-            'Invalid command. The system only accepts the following requests: ls, rm, put, get, mkdir, rmdir, and cat.')
+            'Invalid command. The system only accepts the following requests: ls, rm, put, get, mkdir, rmdir, cat, and fs.')
     # build connection to server and send command line arguments to the server
     s = ' '.join(argvs)
     msg = s.encode('utf-8')
@@ -111,7 +124,7 @@ def main(argvs):
     client.send(msg)
     # waiting for server's response, non-empty responses will be printed
     while True:
-        msg_len = client.recv(30).decode('utf-8')
+        msg_len = client.recv(30).decode('utf-8', 'ignore')
         if msg_len:
             msg_len = int(msg_len)
             if msg_len == -1:
@@ -120,7 +133,7 @@ def main(argvs):
                 received = 0
                 s = ''
                 while received < msg_len:
-                    msg = client.recv(2048).decode('utf-8')
+                    msg = client.recv(2048).decode('utf-8', 'ignore')
                     received += 2048
                     s += msg
                 print(s)
@@ -132,4 +145,6 @@ if __name__ == '__main__':
     if len(sys.argv) <= 2 or len(sys.argv) > 4:
         raise ValueError(
             'Invalid command.')
+    p = subprocess.Popen(["python", "server.py"])
+    time.sleep(0.8)
     main(sys.argv[1:])
