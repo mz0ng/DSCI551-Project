26adf8e2842fec1d720c9bf6234477711a4d240f
mz0ng <110255630+mz0ng@users.noreply.github.com>
2023-05-09 16:49:12 -0700

    Update edfs.py

--- edfs.py
+++ edfs.py
@@ -17,104 +17,134 @@ def main(argvs):
     # process command case by case
     if argvs[0] == '-ls':
         if len(argvs) > 2:
-            raise ValueError(
-                'Please follow the format: -ls [path to directory on server]')
+            print(
+                'Error: Please follow the format: -ls [path to directory on server]')
+            client.close()
+            sys.exit()
         else:
             f = argvs[1]
             ind = f.rfind('.')
             if ind != -1:
-                raise ValueError(
-                    'Please specify path to a directory instead of file.')
+                print('Error: Please specify path to a directory instead of file.')
+                client.close()
+                sys.exit()
     elif argvs[0] == '-rm':
         if len(argvs) > 2:
-            raise ValueError('Please follow the format: -rm [path to file]')
+            print('Error: Please follow the format: -rm [path to file]')
+            client.close()
+            sys.exit()
         else:
             f = argvs[1]
             ind = f.rfind('.')
             if ind == -1:
-                raise ValueError(
-                    'Please specify path to a file instead of directory.')
+                print('Error: Please specify path to a file instead of directory.')
+                client.close()
+                sys.exit()
     elif argvs[0] == '-put':
         if len(argvs) != 3:
-            raise ValueError(
-                'Please follow the format: -put [path to file on local machine] [path to file on server]')
+            print(
+                'Error: Please follow the format: -put [path to file on local machine] [path to file on server]')
+            client.close()
+            sys.exit()
         else:
             ind1 = argvs[1].rfind('.')
             ind2 = argvs[2].rfind('.')
             if ind1 == -1 or ind2 == -1:
-                raise ValueError(
-                    'Please specify path to a file instead of directory.')
+                print('Error: Please specify path to a file instead of directory.')
+                client.close()
+                sys.exit()
             elif not os.path.isfile(argvs[1]):
-                raise ValueError('Failed to find file on your local machine.')
+                print('Error: Failed to find file on your local machine.')
+                client.close()
+                sys.exit()
             else:
                 exts1 = pathlib.Path(argvs[1]).suffix
                 exts2 = pathlib.Path(argvs[2]).suffix
                 if exts1 != exts2:
-                    raise ValueError(
-                        'Please make sure the file extensions match.')
+                    print('Error: Please make sure the file extensions match.')
+                    client.close()
+                    sys.exit()
     elif argvs[0] == '-get':
         if len(argvs) != 3:
-            raise ValueError(
-                'Please follow the format: -get [path to file on server] [path to file on local machine]')
+            print(
+                'Error: Please follow the format: -get [path to file on server] [path to file on local machine]')
+            client.close()
+            sys.exit()
         else:
             ind1 = argvs[1].rfind('.')
             ind2 = argvs[2].rfind('.')
             ind3 = argvs[2].rfind('/')
             if ind1 == -1 or ind2 == -1:
-                raise ValueError(
-                    'Please specify path to a file instead of directory.')
+                print('Error: Please specify path to a file instead of directory.')
+                client.close()
+                sys.exit()
             elif not os.path.exists(argvs[2][:ind3]):
-                raise ValueError(
-                    'Please specify a valid location to save your file.')
+                print('Error: Please specify a valid location to save your file.')
+                client.close()
+                sys.exit()
             else:
                 exts1 = pathlib.Path(argvs[1]).suffix
                 exts2 = pathlib.Path(argvs[2]).suffix
                 if exts1 != exts2:
-                    raise ValueError(
-                        'Please make sure the file extensions match.')
+                    print('Error: Please make sure the file extensions match.')
+                    client.close()
+                    sys.exit()
     elif argvs[0] == '-mkdir':
         if len(argvs) != 2:
-            raise ValueError(
-                'Please follow the format: -mkdir [path to directory on server]')
+            print(
+                'Error: Please follow the format: -mkdir [path to directory on server]')
+            client.close()
+            sys.exit()
         else:
             f = argvs[1]
             ind = f.rfind('.')
             if ind != -1:
-                raise ValueError(
-                    'Please specify path to a directory instead of file.')
+                print('Error: Please specify path to a directory instead of file.')
+                client.close()
+                sys.exit()
     elif argvs[0] == '-rmdir':
         if len(argvs) != 2:
-            raise ValueError(
-                'Please follow the format: -rmdir [path to directory on server]')
+            print(
+                'Error: Please follow the format: -rmdir [path to directory on server]')
+            client.close()
+            sys.exit()
         else:
             f = argvs[1]
             ind = f.rfind('.')
             if ind != -1:
-                raise ValueError(
-                    'Please specify path to a directory instead of file.')
+                print('Error: Please specify path to a directory instead of file.')
+                client.close()
+                sys.exit()
     elif argvs[0] == '-cat':
         if len(argvs) != 2:
-            raise ValueError(
-                'Please follow the format: -cat [path to file on server]')
+            print(
+                'Error: Please follow the format: -cat [path to file on server]')
+            client.close()
+            sys.exit()
         else:
             f = argvs[1]
             ind = f.rfind('.')
             if ind == -1:
-                raise ValueError(
-                    'Please specify path to a file instead of directory.')
+                print('Error: Please specify path to a file instead of directory.')
+                client.close()
+                sys.exit()
     elif argvs[0] == '-fs':
         if len(argvs) != 2:
-            raise ValueError(
-                'Please follow the format: -fs [path to file on server]')
+            print(
+                'Error: Please follow the format: -fs [path to file on server]')
+            client.close()
+            sys.exit()
         else:
             f = argvs[1]
             ind = f.rfind('.')
             if ind == -1:
-                raise ValueError(
-                    'Please specify path to a file instead of directory.')
+                print('Error: Please specify path to a file instead of directory.')
+                client.close()
+                sys.exit()
     else:
-        raise ValueError(
-            'Invalid command. The system only accepts the following requests: ls, rm, put, get, mkdir, rmdir, cat, and fs.')
+        print('Error: Invalid command. The system only accepts the following requests: ls, rm, put, get, mkdir, rmdir, cat, and fs.')
+        client.close()
+        sys.exit()
     # build connection to server and send command line arguments to the server
     s = ' '.join(argvs)
     msg = s.encode('utf-8')
@@ -137,14 +167,14 @@ def main(argvs):
                     received += 2048
                     s += msg
                 print(s)
+                client.close()
                 break
 
 
 # verify number of arguments and then call the main function
 if __name__ == '__main__':
     if len(sys.argv) <= 2 or len(sys.argv) > 4:
-        raise ValueError(
-            'Invalid command.')
+        print('Error: Invalid command.')
     p = subprocess.Popen(["python", "server.py"])
     time.sleep(0.8)
     main(sys.argv[1:])
